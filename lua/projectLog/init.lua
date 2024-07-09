cd39bae9908d2e6c496768181143830a88a6d367
Matthew Borchert <alphaom34@gmail.com>
2024-07-09 10:31:51 -0500

    what

--- lua/projectLog/init.lua
+++ lua/projectLog/init.lua
@@ -1,3 +1,5 @@
-vim.api.nvim_create_user_command('Yep', function ()
-	print("7")
-end , { nargs = 0 })
+function projectLog.setup(self, partial_config)
+	vim.api.nvim_create_user_command('Yep', function ()
+		print("7")
+	end , { nargs = 0 })
+end
