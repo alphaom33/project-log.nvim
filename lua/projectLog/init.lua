dce7de5359920db93d49fc0e0c9b883ccd887799
Matthew Borchert <alphaom34@gmail.com>
2024-07-09 10:37:18 -0500

    wha

--- lua/projectLog/init.lua
+++ lua/projectLog/init.lua
@@ -1,5 +1,24 @@
-function projectLog.setup(self, partial_config)
+local Project_Log = {}
+Project_Log.__index = Project_Log
+
+function Project_Log:new()
+	print("7")
+end
+
+
+local the_project_log = Project_Log:new()
+
+function Project_Log.setup(self, partial_config)
+	if self ~= the_project_log then
+		partial_config = self
+		self = the_project_log
+	end
+
 	vim.api.nvim_create_user_command('Yep', function ()
 		print("7")
 	end , { nargs = 0 })
+
+	return self
 end
+
+return the_project_log
